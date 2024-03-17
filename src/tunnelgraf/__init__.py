db2d6a072bc1e7a85c1d32b88100746f2ba027f7
Dennis Walker <denniswalker@me.com>
2024-03-17 16:30:12 -0600

    Adds the show subcommand for printing the processed configuration to json for external integration.

--- src/tunnelgraf/__init__.py
+++ src/tunnelgraf/__init__.py
@@ -2,6 +2,8 @@ import click
 from tunnelgraf.tunnels import Tunnels
 from io import TextIOWrapper
 from importlib.metadata import version
+import sys
+import json
 
 
 @click.group()
@@ -19,3 +21,32 @@ def cli(ctx) -> None:
 @click.argument("config_file", envvar="TUNNELGRAF_CONFIG", type=click.File("r"))
 def connect(config_file: TextIOWrapper) -> None:
     Tunnels(config_file)
+
+
+@cli.command(help="Print the resulting tunnels configuration w/o connecting.")
+@click.argument("config_file", envvar="TUNNELGRAF_CONFIG", type=click.File("r"))
+@click.option(
+    "--tunnel-id",
+    "-t",
+    help="Print the resulting configuration of the specified tunnel id.",
+    default=None,
+)
+@click.option(
+    "--show-credentials",
+    help="Print the credentials used when connecting. Default: false",
+    is_flag=True,
+    default=False,
+)
+def show(config_file: TextIOWrapper, tunnel_id: str, show_credentials: bool) -> None:
+    tunnels = Tunnels(
+        config_file, connect_tunnels=False, show_credentials=show_credentials
+    ).tunnel_configs
+    if tunnel_id:
+        try:
+            this_tunnel = [tunnel for tunnel in tunnels if tunnel_id == tunnel["id"]][0]
+        except IndexError:
+            print(f"Tunnel id {tunnel_id} not found.")
+            sys.exit(1)
+        print(json.dumps(this_tunnel))
+    else:
+        print(json.dumps(tunnels))
--- src/tunnelgraf/tunnels.py
+++ src/tunnelgraf/tunnels.py
@@ -16,25 +16,41 @@ class Tunnels:
     and binding it to a local port, as specified in the provided config file.
     """
 
-    def __init__(self, config_file: TextIOWrapper):
-        self._config_file: TextIOWrapper = config_file.read()
+    def __init__(
+        self,
+        config_file: TextIOWrapper,
+        connect_tunnels: bool = True,
+        show_credentials: bool = False,
+    ):
+        self._config_file: str = config_file.read()
         self.config: dict | list = self._get_config()
         self.tunnel_defs: TunnelDefinition | list[TunnelDefinition] = TunnelDefinition(
             **self.config
         )
+        self._excluded_fields: list[str] = [
+            "nexthop",
+            "nexthops",
+            "localbindaddress",
+            "localbindport",
+        ]
+        if not show_credentials:
+            self._excluded_fields += ["sshuser", "sshpass", "sshkeyfile"]
         self.tunnels: list[TunnelBuilder] = []
+        self.tunnel_configs: list[TunnelDefinition] = []
         self.original_hosts = Hosts()
         self.new_hosts = Hosts()
+        self._connect_tunnels: bool = connect_tunnels
         self.make_tunnels()
-        self._write_changes_to_hosts_file()
-        try:
-            print("Tunnels started. Press Ctrl-C to stop.")
-            # INFO: on mac, check open ports 'netstat -anvp tcp | awk 'NR<3 || /LISTEN/'
-            while True:
-                sleep(5)
-        except KeyboardInterrupt:
-            print("Closing tunnels...")
-            self.stop_tunnels()
+        if self._connect_tunnels:
+            self._write_changes_to_hosts_file()
+            try:
+                print("Tunnels started. Press Ctrl-C to stop.")
+                # INFO: on mac, check open ports 'netstat -anvp tcp | awk 'NR<3 || /LISTEN/'
+                while True:
+                    sleep(5)
+            except KeyboardInterrupt:
+                print("Closing tunnels...")
+                self.stop_tunnels()
 
     @property
     def config_file(self):
@@ -54,13 +70,11 @@ class Tunnels:
         """Returns the config file."""
         return yaml.load(self.config_file, Loader=yaml.FullLoader)
 
-    def _update_bastion_address(
-        self, nexthop_config: TunnelDefinition, this_parent_tunnel: TunnelBuilder
-    ):
+    def _update_bastion_address(self, nexthop_config: TunnelDefinition):
         """Updates the bastion address and port to reference the one just created."""
         nhcfg = nexthop_config
-        nhcfg.host = this_parent_tunnel.tunnel.local_bind_address[0]
-        nhcfg.port = this_parent_tunnel.tunnel.local_bind_address[1]
+        nhcfg.host = nexthop_config.localbindaddress
+        nhcfg.port = nexthop_config.localbindport
         return nhcfg
 
     def _write_changes_to_hosts_file(self):
@@ -74,13 +88,16 @@ class Tunnels:
             )
             sys.exit(1)
 
-    def _add_to_hosts(self, what_for: str, hosts: list, this_host: str = None) -> None:
+    def _add_to_hosts(
+        self, what_for: str, hosts: list, this_host: str | None = None
+    ) -> None:
         """Adds a host to the hosts list."""
         if this_host is not None:
             hosts.append(this_host)
         if len(hosts) > 0:
             for host in hosts:
-                print(f"Adding {host} to hosts file.")
+                if self._connect_tunnels:
+                    print(f"Adding {host} to hosts file.")
                 this_entry = HostsEntry(
                     address="127.0.0.1",
                     names=[host],
@@ -99,34 +116,37 @@ class Tunnels:
 
     def make_tunnel(self, this_tunnel_def: TunnelDefinition):
         """Creates a tunnel from the provided config."""
+        self._add_to_processed_configs(this_tunnel_def)
         if this_tunnel_def.nexthop is not None:
-            # print(f"DW: Creating tunnel {this_tunnel_def}...")
-            self.tunnels.append(TunnelBuilder(this_tunnel_def))
+            if self._connect_tunnels:
+                self.tunnels.append(TunnelBuilder(this_tunnel_def))
+                tc = self.tunnels[-1].tunnel
+                print(
+                    f"Created tunnel {this_tunnel_def.id}: {tc.local_bind_host}:{tc.local_bind_port} to {tc._remote_binds[0][0]}:{tc._remote_binds[0][1]}"
+                )
             self._add_to_hosts(
                 this_tunnel_def.nexthop.id,
                 this_tunnel_def.nexthop.hosts_file_entries,
                 this_tunnel_def.nexthop.hosts_file_entry,
             )
-            tc = self.tunnels[-1].tunnel
 
-            print(
-                f"Created tunnel {this_tunnel_def.id}: {tc.local_bind_host}:{tc.local_bind_port} to {tc._remote_binds[0][0]}:{tc._remote_binds[0][1]}"
-            )
-            nexthop_config = self._update_bastion_address(
-                this_tunnel_def.nexthop, self.tunnels[-1]
-            )
+            nexthop_config = self._update_bastion_address(this_tunnel_def.nexthop)
             self.make_tunnel(nexthop_config)
 
         if this_tunnel_def.nexthops is not None:
-            this_parent_tunnel = self.tunnels[-1]
             for tunnel in this_tunnel_def.nexthops:
                 nexthop_config = this_tunnel_def
                 nexthop_config.nexthop = tunnel
                 nexthop_config.nexthops = None
-                nexthop_config = self._update_bastion_address(
-                    nexthop_config, this_parent_tunnel
-                )
+                nexthop_config = self._update_bastion_address(nexthop_config)
                 self.make_tunnel(nexthop_config)
+
+    def _add_to_processed_configs(self, this_tunnel_def: TunnelDefinition):
+        """Adds a tunnel to the processed configs."""
+        if not any(this_tunnel_def.id == t["id"] for t in self.tunnel_configs):
+            self.tunnel_configs.append(
+                this_tunnel_def.model_dump(exclude=self._excluded_fields)
+            )
 
     def stop_tunnels(self):
         """Stops all tunnels."""

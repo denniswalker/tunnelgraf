bb5c8682a995163a37d15f87a82f2b775da217f9
Dennis Walker <denniswalker@me.com>
2024-11-06 15:46:24 -0800

    Adds other ssh key types.

--- src/tunnelgraf/run_remote.py
+++ src/tunnelgraf/run_remote.py
@@ -1,4 +1,5 @@
 import paramiko
+from paramiko import RSAKey, DSSKey, ECDSAKey, Ed25519Key
 import pydantic
 from typing import Optional
 
@@ -12,9 +13,9 @@ class RunCommand(pydantic.BaseModel):
 
     def __init__(self, **data):
         super().__init__(**data)
-        pkey: paramiko.PKey | None = None
+        self._pkey: paramiko.PKey | None = None
         if self.identityfile:
-            pkey = paramiko.pkey.from_private_key_file(self.identityfile)
+            self.load_private_key(filename=self.identityfile)
         self._client = paramiko.SSHClient()
         self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
@@ -22,12 +23,12 @@ class RunCommand(pydantic.BaseModel):
                 self.host,
                 username=self.user,
                 password=self.password,
-                pkey=pkey,
+                pkey=self._pkey,
                 port=self.port,
             )
-        except paramiko.ssh_exception.AuthenticationException:
+        except paramiko.AuthenticationException:
             raise ValueError("Authentication failed")
-        except paramiko.ssh_exception.NoValidConnectionsError:
+        except paramiko.SSHException:
             raise ValueError("No valid connection")
 
     def run(self, command):
@@ -40,6 +41,20 @@ class RunCommand(pydantic.BaseModel):
             raise ValueError(f"No result found for: {command}")
         self._client.close()
         return this_result
+
+    def load_private_key(self, filename, password=None):
+        key_classes = [RSAKey, DSSKey, ECDSAKey, Ed25519Key]
+        for key_class in key_classes:
+            try:
+                # Attempt to load the key using the current key class
+                self._pkey = key_class.from_private_key_file(
+                    filename=filename, password=password
+                )
+                print(f"Loaded key using {key_class.__name__}")
+            except Exception as e:
+                # Print the exception for debugging purposes
+                # print(f"Tried to load with {key_class.__name__}: {e}")
+                pass
 
     @property
     def client(self):
